08e4bd582455fd8c4753557ce6384201ad5040ac
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2019-10-19 03:44:41 -0300

    Add navbar
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+
+class Nav extends React.Component {
+    render() {
+        return (
+            <Navbar style={style.nav}>
+                <Navbar.Brand>
+                    <h1 style={style.title}>
+                        Lunar Discovery 
+                        <small class="text-muted" style={style.text}>Helping humanity to reach the stars</small>
+                    </h1>
+                </Navbar.Brand>
+            </Navbar>
+        );
+    }
+}
+
+const style = {
+    nav:{
+        backgroundColor: '#151E3D',
+    },
+    title:{
+        color: 'white',
+        fontSize: 30
+    },
+    text:{
+        marginLeft: 30,
+        fontSize: 20
+    }
+}
+
+export default Nav;
