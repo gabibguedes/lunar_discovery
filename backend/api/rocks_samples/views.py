c6ec18ce9a6dfef8d9cda6efd8514d190b989858
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2019-10-20 09:30:41 -0300

    Added docker compose for api
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>

--- backend/api/rocks_samples/views.py
+++ backend/api/rocks_samples/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from backend.rock_sample.serializers import UserSerializer, GroupSerializer
+from api.rocks_samples.serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
--- backend/backend/rock_sample/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RockSampleConfig(AppConfig):
-    name = 'rock_sample'
